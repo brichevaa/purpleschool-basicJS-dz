39531f6863535aae5c7f19cfa152646fa612343d
Alina Bricheva <brichevaa@MacBook-Pro-user0.local>
2024-05-15 13:24:40 +0300

    change 7-converter

--- code-review-2/7-converter/script.js
+++ code-review-2/7-converter/script.js
@@ -1,14 +1,39 @@
 // функция конвертации
 
 function converter(sum, currency, targetCurrency) {
-   if (currency == 'rub' && targetCurrency == 'dollar') {
-      return sum / 91.78;
-   } else if (currency == 'rub' && targetCurrency == 'euro') {
-      return sum / 98.03;
-   } else if (currency == 'rub' && targetCurrency == 'rub') {
-      return sum;
-   } else {
-      return null;
+   if (currency === 'rub') {
+      switch (targetCurrency) {
+         case 'usd':
+            return sum / 91.36;
+         case 'eur':
+            return sum / 98.67;
+         case 'rub':
+            return 1;
+         default:
+            return 'Ошибка: введите доступные варианты валют';
+      }
+   } else if (currency === 'usd') {
+      switch (targetCurrency) {
+         case 'rub':
+            return sum * 91.36;
+         case 'eur':
+            return sum * 0.9259;
+         case 'usd':
+            return 1;
+         default:
+            return 'Ошибка: введите доступные варианты валют';
+      }
+   } else if (currency === 'eur') {
+      switch (targetCurrency) {
+         case 'rub':
+            return sum * 98.67;
+         case 'usd':
+            return sum / 0.93;
+         case 'eur':
+            return 1;
+         default:
+            return 'Ошибка: введите доступные варианты валют';
+      }
    }
 }
-console.log(converter(1000, 'rub', 'dollar'));
+console.log(converter(100, 'eur', 'usd'));
