995bdad3367a003c9fc418099353d79a323c2127
Alina Bricheva <brichevaa@MacBook-Pro-user0.local>
2024-05-16 12:51:26 +0300

    change 9-sort-loops

--- code-review-2/9-sort-loops/script.js
+++ code-review-2/9-sort-loops/script.js
@@ -1,15 +1,17 @@
 const arr = [1, 40, -5, 10, 0];
 
 function sortArray(array, desc = false) {
-   for (let i = 0; i < array.length; i++) {
-      for (let j = i + 1; j < array.length; j++) {
-         const isExchange = desc ? array[i] > array[j] : array[i] < array[j];
+   const array2 = [...array]; // копируем массив
+   console.log(array2);
+   for (let i = 0; i < array2.length; i++) {
+      for (let j = i + 1; j < array2.length; j++) {
+         const isExchange = desc ? array2[i] > array2[j] : array2[i] < array2[j];
          if (isExchange) {
-            [array[i], array[j]] = [array[j], array[i]];
+            [array2[i], array2[j]] = [array2[j], array2[i]];
          }
       }
    }
-   return array;
+   return array2;
 }
 
 console.log(arr);
