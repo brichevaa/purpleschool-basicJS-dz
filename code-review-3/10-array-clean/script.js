a20b919a05719cc1c5fb466984ed9255087cf8a8
Alina <alinatimanova@gmail.com>
2024-05-22 13:22:47 +0300

    add js code

---
+++ code-review-3/10-array-clean/script.js
@@ -0,0 +1,22 @@
+function arrayClean(arr, deleteArg) {
+   const arrayFilter = [];
+   for (let i = 0; i < arr.length; i++) {
+      if (deleteArg(arr[i])) {
+         continue;
+      }
+      arrayFilter.push(arr[i]);
+   }
+   return arrayFilter;
+}
+
+function deleteArg(num) {
+   if (num > 5) {
+      return true;
+   } else {
+      return false;
+   }
+}
+const test = [-7, 4, 9, 2, 3, 6];
+const test2 = [3, 6, 9, 2];
+const result = arrayClean(test2, deleteArg);
+console.log(result);
