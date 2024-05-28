ea124779d2ecc8709e3f22b83ced99b0e82ac449
Alina <alinatimanova@gmail.com>
2024-05-28 09:41:26 +0300

    add new folder 11-date-array

---
+++ code-review-3/11-date-array/script.js
@@ -0,0 +1,13 @@
+const array = ['10-02-2022', '11/12/2023', '00/13/2022', '41/12/2023'];
+function getDate(arr) {
+   // console.log(arr.sort());
+   // arr.map((el, i) => {
+   //    console.log(el[0] + el[1]);
+   //    if (el[0] + el[1] > 31 || el[3] + el[4] > 12) {
+   //       console.log(false);
+   //    } else {
+   //       console.log(true);
+   //    }
+   // });
+}
+getDate(array);
