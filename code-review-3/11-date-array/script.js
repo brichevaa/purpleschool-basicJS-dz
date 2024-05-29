f65b1e633bf16b9e8797ee18326457f077abb08b
Alina <alinatimanova@gmail.com>
2024-05-29 10:48:37 +0300

    add js code

--- code-review-3/11-date-array/script.js
+++ code-review-3/11-date-array/script.js
@@ -1,13 +1,24 @@
-const array = ['10-02-2022', '11/12/2023', '00/13/2022', '41/12/2023'];
+const array = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023', '0.13.22', '41.12'];
 function getDate(arr) {
-   // console.log(arr.sort());
-   // arr.map((el, i) => {
-   //    console.log(el[0] + el[1]);
-   //    if (el[0] + el[1] > 31 || el[3] + el[4] > 12) {
-   //       console.log(false);
-   //    } else {
-   //       console.log(true);
-   //    }
-   // });
+   const newArr = [];
+   const filteredArr = [];
+   arr.map((el) => {
+      if (el.length > 9) {
+         newArr.push(el);
+      }
+   });
+   console.log(newArr);
+   newArr.map((elem) => filteredArr.push(elem.split('/').join('-')));
+   const newArr2 = filteredArr.map((el) => {
+      const a = el.split('-')[0];
+      console.log(a);
+
+      if (a > 0 && a <= 31) {
+         return el;
+      }
+   });
+   console.log(newArr2);
+   const result = newArr2.sort();
+   console.log(result);
 }
 getDate(array);
