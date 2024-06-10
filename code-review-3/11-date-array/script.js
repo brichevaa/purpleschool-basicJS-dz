8d229e91124298a0ac0522f42bc19cc769b3a086
Alina <alinatimanova@gmail.com>
2024-06-10 15:40:03 +0300

    change js file 11-date-array

--- code-review-3/11-date-array/script.js
+++ code-review-3/11-date-array/script.js
@@ -1,24 +1,33 @@
-const array = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023', '0.13.22', '41.12'];
+const array = [
+   '10-02-2022',
+   'тест',
+   '11/12/2023',
+   '00/13/2022',
+   '41/12/2023',
+   '0.13.22',
+   '41.12',
+   '10-00-2022',
+   '12.13.2022',
+   '00.13/2022',
+];
 function getDate(arr) {
-   const newArr = [];
-   const filteredArr = [];
-   arr.map((el) => {
-      if (el.length > 9) {
-         newArr.push(el);
-      }
-   });
-   console.log(newArr);
-   newArr.map((elem) => filteredArr.push(elem.split('/').join('-')));
-   const newArr2 = filteredArr.map((el) => {
-      const a = el.split('-')[0];
-      console.log(a);
+   const filteredArr = arr.filter((el) => el.length == 10).map((el) => el.split('/').join('-'));
+   const result = [];
+   for (let i = 0; i < filteredArr.length; i++) {
+      const parts = filteredArr[i].split('-');
 
-      if (a > 0 && a <= 31) {
-         return el;
+      if (
+         parts[0] > 0 &&
+         parts[0] <= 31 &&
+         parts[0].length === 2 &&
+         parts[1].length === 2 &&
+         parts[1] <= 12 &&
+         parts[1] > 0 &&
+         parts[2].length === 4
+      ) {
+         result.push(filteredArr[i]);
       }
-   });
-   console.log(newArr2);
-   const result = newArr2.sort();
-   console.log(result);
+   }
+   return result;
 }
-getDate(array);
+console.log(getDate(array));
