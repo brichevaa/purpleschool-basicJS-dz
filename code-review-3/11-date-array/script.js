7594ac35a246d304f41e0cce022a01e7d9603e6b
Alina <alinatimanova@gmail.com>
2024-06-17 13:18:16 +0300

    change js file

--- code-review-3/11-date-array/script.js
+++ code-review-3/11-date-array/script.js
@@ -1,31 +1,72 @@
 const array = [
    '10-02-2022',
    'тест',
-   '11/12/2023',
+   '02/29/2000',
+   '02/28/2000',
+   '02/29/2001',
+   '02/28/2001',
    '00/13/2022',
    '41/12/2023',
+   '10/32/2023',
+   '12/06/2023',
    '0.13.22',
    '41.12',
-   '10-00-2022',
+   '10-01-2022',
    '12.13.2022',
    '00.13/2022',
 ];
 function getDate(arr) {
-   const filteredArr = arr.filter((el) => el.length == 10).map((el) => el.split('/').join('-'));
-   const result = [];
-   for (let i = 0; i < filteredArr.length; i++) {
-      const parts = filteredArr[i].split('-');
+   const filteredArr = arr.filter((el) => el.length == 10);
+   console.log(filteredArr);
 
-      if (
-         parts[0] > 0 &&
-         parts[0] <= 31 &&
-         parts[0].length === 2 &&
-         parts[1].length === 2 &&
-         parts[1] <= 12 &&
-         parts[1] > 0 &&
-         parts[2].length === 4
-      ) {
-         result.push(filteredArr[i]);
+   const result = [];
+
+   // проверка на високосность года
+   const getLeapYear = (year) => year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+
+   for (let i = 0; i < filteredArr.length; i++) {
+      if (filteredArr[i].includes('/')) {
+         const innerElement = filteredArr[i].split('/');
+         let [month, day, year] = innerElement;
+         if (getLeapYear(year) && month === '02' && day <= 29) {
+            let arr = [];
+            arr.push(day, month, year);
+            result.push(arr.join('-'));
+         } else if (
+            !getLeapYear(year) &&
+            month === '02' &&
+            day <= 28 &&
+            day > 0 &&
+            day.length === 2 &&
+            month.length === 2 &&
+            month <= 12 &&
+            month > 0
+         ) {
+            let arr = [];
+            arr.push(day, month, year);
+            result.push(arr.join('-'));
+         } else if (
+            !getLeapYear(year) &&
+            month !== '02' &&
+            day <= 31 &&
+            day > 0 &&
+            day.length === 2 &&
+            month.length === 2 &&
+            month <= 12 &&
+            month > 0
+         ) {
+            let arr = [];
+            arr.push(day, month, year);
+            result.push(arr.join('-'));
+         }
+      } else if (filteredArr[i].includes('-')) {
+         const innerElement = filteredArr[i].split('-');
+         const [day, month, year] = innerElement;
+         if (day > 0 && day <= 31 && day.length === 2 && month <= 12 && month > 0) {
+            let arr = [];
+            arr.push(day, month, year);
+            result.push(arr.join('-'));
+         }
       }
    }
    return result;
